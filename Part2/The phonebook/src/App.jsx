7d90340a1f0627762f232638ae88b9f17e17b3f0
Will Loughlin <will.loughlin@gmail.com>
2025-04-28 17:10:23 +0100

    Implement collection of initial data from db.json using Axios effect hook. (Exercise 2.11)

--- Part2/The phonebook/src/App.jsx
+++ Part2/The phonebook/src/App.jsx
@@ -1,4 +1,62 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addContact = (event) => {
+    event.preventDefault()
+    if(persons.find((el) => el.name === newName)){
+      window.alert(`${newName} is already added to phonebook`)
+      return
+    }
+    const newPerson = {
+      name:newName,
+      number:newNumber,
+      id:persons.length + 1
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const inputHandler = (event) => {
+    switch (event.target.className) {
+      case 'newName':
+        setNewName(event.target.value)
+        break
+      case 'newNumber':
+        setNewNumber(event.target.value)
+        break
+      case 'filter':
+        setNewFilter(event.target.value)
+        break
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter inputVal={newFilter} handler={inputHandler}/>
+      <h3>Add new contact</h3>
+      <PersonForm name={newName} number={newNumber} onSubmit={addContact} inputHandler={inputHandler}/>
+      <h2>Contacts</h2>
+      <GetContacts persons={persons} filter={newFilter}/>
+    </div>
+  )
+}
 
 const GetContacts = ({persons, filter}) => {
   const lowerCaseQuery = filter ? filter.toLowerCase() : null
@@ -40,59 +98,6 @@ const PersonForm = ({name, number, onSubmit, inputHandler}) => {
         <button type="submit">add</button>
       </div>
     </form>
-  )
-}
-
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('')
-
-  const addContact = (event) => {
-    event.preventDefault()
-    if(persons.find((el) => el.name === newName)){
-      window.alert(`${newName} is already added to phonebook`)
-      return
-    }
-    const newPerson = {
-      name:newName,
-      number:newNumber,
-      id:persons.length + 1
-    }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
-  }
-
-  const inputHandler = (event) => {
-    switch (event.target.className) {
-      case 'newName':
-        setNewName(event.target.value)
-        break
-      case 'newNumber':
-        setNewNumber(event.target.value)
-        break
-      case 'filter':
-        setNewFilter(event.target.value)
-        break
-    }
-  }
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter inputVal={newFilter} handler={inputHandler}/>
-      <h3>Add new contact</h3>
-      <PersonForm name={newName} number={newNumber} onSubmit={addContact} inputHandler={inputHandler}/>
-      <h2>Contacts</h2>
-      <GetContacts persons={persons} filter={newFilter}/>
-    </div>
   )
 }
 
