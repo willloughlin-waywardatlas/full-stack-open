b7c27358a48479c48e7b9794a44ce550b6a48a91
Will Loughlin <will.loughlin@gmail.com>
2025-04-29 16:03:24 +0100

    Completion of exercises 2.12 - 2.15 and minor best practice improvements to input change handler now handleInputChange()

--- Part2/The phonebook/src/App.jsx
+++ Part2/The phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/person'
 
 
 const App = () => {
@@ -9,8 +9,8 @@ const App = () => {
   const [newFilter, setNewFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -18,22 +18,50 @@ const App = () => {
 
   const addContact = (event) => {
     event.preventDefault()
-    if(persons.find((el) => el.name === newName)){
-      window.alert(`${newName} is already added to phonebook`)
-      return
-    }
     const newPerson = {
       name:newName,
       number:newNumber,
-      id:persons.length + 1
     }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
+    const existingContact = persons.find((el) => el.name === newName)
+    if(existingContact){
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        personService
+          .update(existingContact.id,newPerson)
+          .then(response => {
+            setPersons(persons.map(p => p.id === existingContact.id ? response.data : p))
+            setNewName('')
+            setNewNumber('')
+          })
+      }
+    } else {
+      personService
+        .create(newPerson)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
+    }
   }
 
-  const inputHandler = (event) => {
-    switch (event.target.className) {
+  const removeContact = (target) => {
+    if (window.confirm(`Delete ${target.name}?`)) {
+      personService
+        .remove(target.id)
+        .then(response => {
+          setPersons(persons.filter(p => p.id !== target.id))
+        })
+        .catch(error => {
+          alert(
+            `The contact '${target.name}' has already been deleted from the server`
+          )
+          setPersons(persons.filter(p => p.id !== target.id))
+        })
+    }
+  }
+
+  const handleInputChange = (event) => {
+    switch (event.target.name) {
       case 'newName':
         setNewName(event.target.value)
         break
@@ -49,16 +77,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter inputVal={newFilter} handler={inputHandler}/>
+      <Filter inputVal={newFilter} handleInputChange={handleInputChange}/>
       <h3>Add new contact</h3>
-      <PersonForm name={newName} number={newNumber} onSubmit={addContact} inputHandler={inputHandler}/>
+      <PersonForm name={newName} number={newNumber} onSubmit={addContact} handleInputChange={handleInputChange}/>
       <h2>Contacts</h2>
-      <GetContacts persons={persons} filter={newFilter}/>
+      <GetContacts persons={persons} filter={newFilter} removeContact={removeContact}/>
     </div>
   )
 }
 
-const GetContacts = ({persons, filter}) => {
+const GetContacts = ({persons, filter, removeContact}) => {
   const lowerCaseQuery = filter ? filter.toLowerCase() : null
   const peopleToShow = 
     filter ? 
@@ -66,7 +94,9 @@ const GetContacts = ({persons, filter}) => {
     :[...persons]
   const contacts  = peopleToShow.map((person) => {
     return (
-      <p key={person.id}>Name: {person.name} {person.number}</p>
+      <div key={person.id}>
+        {person.name} {person.number} <button onClick={() => removeContact(person)}>Delete</button>
+      </div>
     )
   });
 
@@ -77,22 +107,22 @@ const GetContacts = ({persons, filter}) => {
   )
 }
 
-const Filter = ({inputVal, handler}) => {
+const Filter = ({inputVal, handleInputChange}) => {
   return(
     <div>
-      Search contact names which contain <input value={inputVal} onChange={handler} className='filter'/>
+      Search contact names which contain <input value={inputVal} onChange={handleInputChange} name='filter'/>
     </div>
   )
 }
 
-const PersonForm = ({name, number, onSubmit, inputHandler}) => {
+const PersonForm = ({name, number, onSubmit, handleInputChange}) => {
   return(
     <form onSubmit={onSubmit}>
       <div>
-        Name: <input value={name} onChange={inputHandler} className='newName'/>
+        Name: <input value={name} onChange={handleInputChange} name='newName'/>
       </div>
       <div>
-        Number: <input value={number} onChange={inputHandler} className='newNumber'/>
+        Number: <input value={number} onChange={handleInputChange} name='newNumber'/>
       </div>
       <div>
         <button type="submit">add</button>
